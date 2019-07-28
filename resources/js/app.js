f068db9fc6588d13bc9a7b6d20ecfd7450f51cdb
Brandon Park <brandonpark206@gmail.com>
2019-07-27 21:35:29 -0700

    adding vuex modules to vue instance

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,9 +31,10 @@ if (token) {
 }
 
 import Vue            from 'vue';
-import router           from './routes.js'
-
+import router           from './routes.js';
+import store from './store.js';
 
 new Vue({
-    router
+    router,
+    store,
 }).$mount('#app')
---
+++ resources/js/store.js
@@ -0,0 +1,29 @@
+/*
+  Adds the promise polyfill for IE 11
+*/
+require('es6-promise').polyfill();
+
+/*
+    Imports Vue and Vuex
+*/
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+/*
+    Initializes Vuex on Vue.
+*/
+Vue.use( Vuex )
+
+/*
+    Imports all of the modules used in the application to build the data store.
+*/
+import { cafes } from './modules/cafes.js'
+
+/*
+  Exports our data store.
+*/
+export default new Vuex.Store({
+    modules: {
+        cafes
+    }
+});
